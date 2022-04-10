a11024773efd8d8092b54687b404aad3cbedc804
Kenneth Watkins <vercore5866@gmail.com>
2022-04-10 15:35:40 -0400

    Backend cabal image building

--- nix/setup.nix
+++ nix/setup.nix
@@ -1,10 +1,35 @@
+# Documentation 
+# - https://github.com/NixOS/nixpkgs/blob/master/pkgs/build-support/docker/examples.nix
+
 { pkgs ? import <nixpkgs> {}
-, pkgsLinux? import <nixpkgs> { system="x86_64-linux"; }
+, pkgsLinux ? import <nixpkgs> { system="x86_64-linux"; }
 }:
 
-pkgs.dockerTools.buildImage {   
-  name = "hello-docker";
-  config = {
-    Cmd = [ "${pkgsLinux.hello}/bin/hello" ];
-  };
+rec {
+  # https://github.com/Gabriel439/haskell-nix
+   backend = pkgs.dockerTools.buildImage {
+
+     name = "backend";
+     tag = "latest";
+     created = "now";
+
+     contents = [
+       pkgs.bash
+       pkgs.cabal2nix
+       pkgs.nix-prefetch-git
+       pkgs.cabal-install
+     ];
+
+     runAsRoot = '' 
+       mkdir -p /backend
+     '';
+
+     config = {
+       Cmd = [ "${pkgs.bash}/bin/bash" "-c" "cabal --version" ];
+       WorkingDir = "/backend";
+       Volumes = {
+         "/backend" = {};
+       };
+     };
+   };
 }
